6a7d55ac201487494631087299eb4f2a7b26e3de
Gautham C Sudheer <csgautham091003@gmail.com>
2024-02-16 00:33:34 +0530

    Updated Portfolio

--- main.js
+++ main.js
@@ -1,54 +1,33 @@
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 40);
-  })
+})
 
-  var typed = new Typed('.auto-type', {
-    strings: ["web developer.", "teacher.", "UI designer."],
-    typeSpeed:100,
-    backSpeed: 100,
-    loop: true
-  });
-  
-  const carousel = document.querySelector('.carousel');
-  const prevBtn = carousel.querySelector('.prev');
-  const nextBtn = carousel.querySelector('.next');
-  const slides = carousel.querySelectorAll('img');
-  let currentSlide = 0;
-  slides[currentSlide].classList.add('active');
-  
-  prevBtn.addEventListener('click', () => {
-    slides[currentSlide].classList.remove('active');
-    currentSlide--;
-    if (currentSlide < 0) {
-      currentSlide = slides.length - 1;
-    }
-    slides[currentSlide].classList.add('active');
-  });
-  
-  nextBtn.addEventListener('click', () => {
-    slides[currentSlide].classList.remove('active');
-    currentSlide++;
-    if (currentSlide >= slides.length) {
-      currentSlide = 0;
-    }
-    slides[currentSlide].classList.add('active');
-  });
+document.addEventListener("DOMContentLoaded", function() {
+    const homeLink = document.querySelector('a[href="#"]');
+    homeLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
 
-  const menuBtn = document.querySelector('.menuBtn');
-  const closeBtn = document.querySelector('.closeBtn');
-  const navMenu = document.querySelector('header ul');
 
-  // Toggle navigation menu on click of menu button
-  menuBtn.addEventListener('click', () => {
-    navMenu.classList.add('show');
-    menuBtn.style.display = 'none';
-    closeBtn.style.display = 'block';
-  });
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('header ul li a');
 
-  // Hide navigation menu on click of close button
-  closeBtn.addEventListener('click', () => {
-    navMenu.classList.remove('show');
-    closeBtn.style.display = 'none';
-    menuBtn.style.display = 'block';
-  });
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
